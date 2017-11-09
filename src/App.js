c38317c870abab35c6c08fc7a3a7707c3e45df66
githubowyprojekcik <githubowyprojekcik@gmail.com>
2017-11-09 19:37:00 +0100

    bomba

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import DatePicker from 'material-ui/DatePicker';
+import Paper from 'material-ui/Paper';
+import Slider from 'material-ui/Slider';
 import './App.css';
 
 class App extends Component {
@@ -10,11 +12,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <div>Click to pick a date:</div>
+          
           <button class="date-pick-button" value="Pick Date">
             
-          <DatePicker/>
           </button>
+         
         </div>
+          <Slider />   
       </div>
       </MuiThemeProvider>
     );
